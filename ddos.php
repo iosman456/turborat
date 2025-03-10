a821377e25a3fa21d917869ddb4a0003b1bd59af
iosman456 <muhammetcanaltan41@gmail.com>
2025-03-10 17:32:53 +0300

    Update ddos.php

--- ddos.php
+++ ddos.php
@@ -1,1 +1,16 @@
+<?php
+// Figlet komutunu tanımla
+$command = "figlet FLK";
 
+// Komutu çalıştır ve çıktısını al
+$output = [];
+$return_var = 0;
+exec($command, $output, $return_var);
+
+// Çıktıyı ekrana yazdır
+if ($return_var === 0) {
+    echo implode("\n", $output);
+} else {
+    echo "Figlet komutu çalıştırılırken bir hata oluştu.\n";
+}
+?>
